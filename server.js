1f30f1369532292fb71c00ce4c98bcc2ab27f8c2
Dheya Madhani <dheyam@gmail.com>
2022-11-04 23:21:01 -0400

    simplified code

--- server.js
+++ server.js
@@ -12,15 +12,17 @@ const port = args.port ? args.port: 5000;
 
 
 app.get('/app', (req, res, next) => {
-	res.statusCode = 200;
-    res.statusMessage = "OK"
-    res.writeHead(res.statusCode, { 'Content-Type' : 'text/plain' });
-    res.end(res.statusCode+' '+res.statusMessage)
+	res.send("200 OK").end()
+    // res.statusCode = 200;
+    // res.statusMessage = "OK"
+    // res.writeHead(res.statusCode, { 'Content-Type' : 'text/plain' });
+    // res.end(res.statusCode+' '+res.statusMessage)
 })
 
 app.get('/app/roll', (req, res, next) => {
 	res.status(200);
-    res.json(JSON.stringify(roll(6, 2, 1))).end();
+    res.send(roll(6, 2, 1)).end();
+    // res.json(JSON.stringify(roll(6, 2, 1))).end();
 })
 
 app.get('/app/roll', (req, res, next) => {
@@ -29,29 +31,32 @@ app.get('/app/roll', (req, res, next) => {
     let roll = req.body.rolls || req.query.rolls;
 
 	res.status(200);
-    res.json(JSON.stringify(roll(side,dice,roll))).end();
+    res.send(roll(side,dice,roll)).end();
+    // res.json(JSON.stringify(roll(side,dice,roll))).end();
 })
 
 app.get('/app/roll/:sides', (req, res, next) => {
 	res.status(200)
-    res.json(JSON.stringify(roll(req.params.sides, 2, 1))).end();
+    res.send(roll(parseInt(req.params.sides), 2, 1)).end();
+    // res.json(JSON.stringify(roll(parseInt(req.params.sides), 2, 1))).end();
 })
 
 app.get('/app/roll/:sides/:dice', (req, res, next) => {
 	res.status(200);
-    res.json(JSON.stringify(roll(req.params.sides, req.params.dice, 1))).end();
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1)).end();
+    // res.json(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1))).end();
 })
 
 app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
 	res.status(200);
-    res.json(JSON.stringify(roll(req.params.sides, req.params.dice, req.params.rolls))).end();
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls))).end();
+    // res.json(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)))).end();
 })
 
-app.use(function(req, res){
-	res.status(404);
-    res.send("404 NOT FOUND").end();
+app.use((req, res) => {
+	res.status(404).send("404 NOT FOUND").end();
 })
 
-app.listen(port, () => {
-	console.log("Server listening on port" + port)
+app.listen(port, (err) => {
+	console.log("Server listening on port " + port);
 })
