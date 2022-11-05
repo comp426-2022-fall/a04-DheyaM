187a3f15f957ae221b3161ec8142662da3beed8c
Dheya Madhani <dheyam@gmail.com>
2022-11-04 23:37:21 -0400

    cleaned code

--- server.js
+++ server.js
@@ -6,48 +6,38 @@ import minimist from "minimist";
 
 const app = express()
 const args = minimist(process.argv.slice(2));
-
 const port = args.port ? args.port: 5000;
-
-
 
 app.get('/app', (req, res, next) => {
 	res.send("200 OK").end()
 })
 
+app.get('/app/roll', (req, res, next) => {
+	res.status(200);
+    res.send(roll(6, 2, 1)).end();
+})
 
 app.get('/app/roll', (req, res, next) => {
     let side = req.body.sides || req.query.sides;
     let dice = req.body.dice || req.query.dice;
     let roll = req.body.rolls || req.query.rolls;
-
 	res.status(200);
     res.send(roll(side,dice,roll)).end();
-    // res.json(JSON.stringify(roll(side,dice,roll))).end();
 })
 
 app.get('/app/roll/:sides', (req, res, next) => {
 	res.status(200)
     res.send(roll(parseInt(req.params.sides), 2, 1)).end();
-    // res.json(JSON.stringify(roll(parseInt(req.params.sides), 2, 1))).end();
 })
 
 app.get('/app/roll/:sides/:dice', (req, res, next) => {
 	res.status(200);
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1)).end();
-    // res.json(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1))).end();
 })
 
 app.use('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
 	res.status(200);
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls))).end();
-    // res.json(JSON.stringify(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)))).end();
-})
-
-app.get('/app/roll', (req, res, next) => {
-	res.status(200);
-    res.send(roll(6, 2, 1)).end();
-    // res.json(JSON.stringify(roll(6, 2, 1))).end();
 })
 
 app.use((req, res) => {
@@ -57,5 +47,3 @@ app.use((req, res) => {
 app.listen(port, (err) => {
 	console.log("Server port is " + port);
 })
-
-process.exit(0);
