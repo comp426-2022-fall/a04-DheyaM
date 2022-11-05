79f4380282faf6469a9a2733ddfdd7c48258a288
Dheya Madhani <dheyam@gmail.com>
2022-11-04 22:16:08 -0400

    draft of server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+import {roll} from "./lib/roll";
+import express from "express";
+import minimist from "minimist";
+
+const app = express()
+const args = minimist(process.argv.slice(2));
+
+const port = args.port ? args.port: 5000;
+
+app.get('/app', (req, res, next) => {
+	res.status(200).send('OK');
+})
+
+app.get('/app/roll', (req, res, next) => {
+	res.json(JSON.stringify(roll(6, 2, 1)));
+})
+
+app.post('/app/roll', (req, res, next) => {
+	res.status(200).send(JSON.stringify(roll.roll(req.body.sides, req.body.dice, req.body.rolls)));
+})
+
+app.get('/app/roll/:sides', (req, res, next) => {
+	res.status(200).send(JSON.stringify(roll(req.params['sides'], 2, 1)));
+})
+
+app.get('/app/roll/:sides/:dice', (req, res, next) => {
+	res.status(200).send(JSON.stringify(roll(req.params['sides'], req.params['dice'], 1)));
+})
+
+app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
+	res.status(200).send(JSON.stringify(roll(req.params['sides'], req.params['dice'], req.params['rolls'])));
+})
+
+app.get('*', function(req, res){
+	res.status(404).send("NOT FOUND");
+})
+
+app.listen(port, () => {
+	console.log("Server listening on port" + port)
+})
+
+process.exit(0)
