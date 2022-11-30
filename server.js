3701f7075d21b0b83037d186b274fe43f7e46bc0
Dheya Madhani <dheyam@gmail.com>
2022-11-30 16:02:19 -0500

    fixed post

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ const app = express()
 const args = minimist(process.argv.slice(2));
 const port = args.port ? args.port: 5000;
 
-app.post('/app', (req, res) => {
+app.get('/app', (req, res) => {
 	res.send("200 OK");
     res.end()
 })
 
-app.post('/app/roll', (req, res, next) => {
+app.get('/app/roll', (req, res, next) => {
     res.send(Roll(6, 2, 1));
     res.end()
 })
 
-app.post('/app/roll/:sides', (req, res, next) => {
+app.get('/app/roll/:sides', (req, res, next) => {
     res.send(Roll(parseInt(req.body.sides), 2, 1));
     res.end()
 })
 
-app.post('/app/roll/:sides/:dice', (req, res, next) => {
+app.get('/app/roll/:sides/:dice', (req, res, next) => {
     res.send(Roll(parseInt(req.body.sides), parseInt(req.body.dice), 1));
     res.end()
 })
@@ -34,7 +34,7 @@ app.post('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
 })
 
 
-app.post('/app/roll', (req, res, next) => {
+app.get('/app/roll', (req, res, next) => {
     var side = req.body.sides ? parseInt(req.body.sides) : 6;
     var dice = req.body.dice ? parseInt(req.body.dice) : 2;
     var roll = req.body.rolls ? parseInt(req.body.rolls) : 1;
@@ -43,7 +43,7 @@ app.post('/app/roll', (req, res, next) => {
 })
 
 
-app.post((req, res, next) => {
+app.get((req, res, next) => {
 	res.status(404).send("404 NOT FOUND");
     res.end()
 })
