63c3046dffcdc1326f99d184ca5593e65983cc22
Dheya Madhani <dheyam@gmail.com>
2022-12-02 19:28:33 -0500

    removed res.end

--- server.js
+++ server.js
@@ -9,45 +9,38 @@ const args = minimist(process.argv.slice(2));
 const port = args.port ? args.port: 5000;
 
 app.get('/app', (req, res) => {
-	res.send("200 OK");
-    res.end()
+    res.send("200 OK");
 })
 
 app.get('/app/roll', (req, res, next) => {
     res.send(Roll(6, 2, 1));
-    res.end()
 })
 
 app.get('/app/roll/:sides', (req, res, next) => {
     res.send(Roll(parseInt(req.params.sides), 2, 1));
-    res.end()
 })
 
 app.get('/app/roll/:sides/:dice', (req, res, next) => {
     res.send(Roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
-    res.end()
 })
 
 app.use('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
     res.send(Roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
-    res.end()
 })
 
 
 app.use('/app/roll', (req, res, next) => {
-    var side = req.params.sides ? parseInt(req.params.sides) : 6;
-    var dice = req.params.dice ? parseInt(req.params.dice) : 2;
-    var roll = req.params.rolls ? parseInt(req.params.rolls) : 1;
+    const side = req.params.sides ? parseInt(req.params.sides) : 6;
+    const dice = req.params.dice ? parseInt(req.params.dice) : 2;
+    const roll = req.params.rolls ? parseInt(req.params.rolls) : 1;
     res.send(Roll(side,dice,roll));
-    res.end()
 })
 
 
-app.use((req, res, next) => {
-	res.status(404).send("404 NOT FOUND");
-    res.end()
+app.use((req, res) => {
+   res.status(404).send("404 NOT FOUND");
 })
 
 app.listen(port, (err) => {
-	console.log("Server port is " + port);
+   console.log("Server port is " + port);
 })
