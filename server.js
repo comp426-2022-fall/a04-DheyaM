9a5c098fe6188ae27aeca8b4f1665ea9fc2efd78
Dheya Madhani <dheyam@gmail.com>
2022-12-04 15:37:12 -0500

    fixed all endpoints

--- server.js
+++ server.js
@@ -8,39 +8,35 @@ const app = express()
 const args = minimist(process.argv.slice(2));
 const port = args.port ? args.port: 5000;
 
+// get information from HTTP body as url encoded
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/app', (req, res) => {
     res.send("200 OK");
 })
 
-app.get('/app/roll', (req, res, next) => {
+// default endpoint
+app.get('/app/roll', (req, res) => {
     res.send(Roll(6, 2, 1));
 })
 
-app.get('/app/roll/:sides', (req, res, next) => {
+app.get('/app/roll/:sides', (req, res) => {
     res.send(Roll(parseInt(req.params.sides), 2, 1));
 })
 
-app.get('/app/roll/:sides/:dice', (req, res, next) => {
+app.get('/app/roll/:sides/:dice', (req, res) => {
     res.send(Roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
 })
 
-app.use('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
+app.get('/app/roll/:sides/:dice/:rolls', (req, res) => {
     res.send(Roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
 })
-
-
-app.use('/app/roll', (req, res, next) => {
-    const side = req.params.sides ? parseInt(req.params.sides) : 6;
-    const dice = req.params.dice ? parseInt(req.params.dice) : 2;
-    const roll = req.params.rolls ? parseInt(req.params.rolls) : 1;
-    res.send(Roll(side,dice,roll));
-})
-
 
 app.use((req, res) => {
    res.status(404).send("404 NOT FOUND");
 })
 
 app.listen(port, (err) => {
-   console.log("Server port is " + port);
-})
+    console.log("Server port is " + port);
+ })
